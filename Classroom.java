72d3b3c40f8919825343f8f98524d33eaf27222e
Carole Chevalier <ateliercakocako@gmail.com>
2019-06-05 16:45:03 +0200

    Classroom

---
+++ Classroom.java
@@ -0,0 +1,10 @@
+class Classroom{
+
+    public static void main(String[] args) {
+        Wilder jeanClaude = new Wilder("Jean Claude", true);
+        Wilder henri = new Wilder("Henri", false);
+
+        jeanClaude.whoAmI();
+        henri.whoAmI();
+    }
+}
